9993c57aa5c2e29aa131dd63e3dc6e496309a81c
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-04-21 12:03:39 +0700

    void

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -95,4 +95,22 @@ public:
         return true;
     }
 
-}    
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+            return;
+        }
+        cout << "\nData di dalam list adalah: \n";
+        Node *current = START;
+
+        while (current != NULL)
+        {
+            cout << current->noMhs << endl;
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
