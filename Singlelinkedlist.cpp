5160d98c59b074e73d0a70031287780bd35d21ae
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-04-21 11:59:16 +0700

    Node

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -40,6 +40,23 @@ public:
             START = Nodebaru;
             return;
         }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak di ijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        Nodebaru->next = current;
+        previous->next = Nodebaru;
     }
 }
-        
+    
